94a8b44fc88eff6b42cf284e33b000de6539ea0b
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2025-02-25 17:19:12 +0100

    Fixed Labb 1 Time

--- Labb 1/Tid/time_client.c
+++ Labb 1/Tid/time_client.c
@@ -5,44 +5,36 @@
 #include <time.h>
 #include <unistd.h>
 
-#define SERVER_PORT 37
-
-// TODO
-#define SERVER_IP "127.0.0.1" // Change this to the actual server IP
+#include "./constants.h"
 
 int main() {
-	int sockfd;
-	struct sockaddr_in server_addr;
-	char request[1] = {0}; // Empty request
-	uint32_t server_time;
-	time_t unix_time;
-
 	// Create a UDP socket
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (socket_fd < 0) {
 		perror("Socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 
 	// Setup server address structure
-	memset(&server_addr, 0, sizeof(server_addr));
+	struct sockaddr_in server_addr = {0};
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(SERVER_PORT);
 	server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
 	// Send an empty request to the server
-	sendto(sockfd, request, sizeof(request), 0, (struct sockaddr *)&server_addr,
+	sendto(socket_fd, NULL, 0, 0, (struct sockaddr *)&server_addr,
 				 sizeof(server_addr));
 
 	// Receive the server's response
-	recvfrom(sockfd, &server_time, sizeof(server_time), 0, NULL, NULL);
+	uint32_t server_time;
+	recv(socket_fd, &server_time, sizeof(server_time), 0);
 
-	// Convert from RFC 868 time to Unix time
 	server_time = ntohl(server_time);
-	unix_time = server_time - 2208988800U;
+	// Convert RFC 868 time back to Unix time
+	time_t unix_time = server_time - SEVENTY_LONG_YEARS;
 
-	// Print the received time in a readable format
 	printf("Server time: %s", ctime(&unix_time));
 
-	close(sockfd);
+	close(socket_fd);
 	return 0;
 }
--- Labb 1/Tid/time_server.c
+++ Labb 1/Tid/time_server.c
@@ -1,60 +1,60 @@
 #include <arpa/inet.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
 
-#define PORT 37 // RFC 868 Time Protocol Port
-#define BUFFER_SIZE 1
+#include "./constants.h"
 
-uint32_t get_time() {
+/** Convert Unix time to RFC 868 time by adding 70 years (1970 -> 1900) */
+static inline uint32_t get_rfc_time() {
 	time_t current_time = time(NULL);
-	return htonl((uint32_t)(current_time +
-													2208988800U)); // Convert Unix time to RFC 868 time
+	return htonl(current_time + SEVENTY_LONG_YEARS);
 }
 
 int main() {
-	int sockfd;
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t addr_len = sizeof(client_addr);
-	char buffer[BUFFER_SIZE]; // Placeholder for an empty request
-	uint32_t response_time;
-
 	// Create a UDP socket
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	int socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (socket_fd < 0) {
 		perror("Socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 
 	// Setup server address structure
-	memset(&server_addr, 0, sizeof(server_addr));
+	struct sockaddr_in server_addr = {0};
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	server_addr.sin_port = htons(SERVER_PORT);
 
 	// Bind the socket to the port
-	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+	int bind_result =
+			bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	if (bind_result < 0) {
 		perror("Bind failed");
-		close(sockfd);
+		close(socket_fd);
 		exit(EXIT_FAILURE);
 	}
 
-	// printf("Time server running on UDP port %d...\n", PORT);
+	while (true) {
+		struct sockaddr_in client_addr;
+		// "should be initialized to the size of the buffer associated with
+		// src_addr" - man 2 recv
+		socklen_t addr_len = sizeof(client_addr);
 
-	while (1) {
-		// Receive request from a client
-		recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr,
-						 &addr_len);
+		// Receive *empty* request from a client
+		recvfrom(socket_fd, NULL, 0, 0, (struct sockaddr *)&client_addr, &addr_len);
+
+		printf("Connection from: %s:%i\n", inet_ntoa(client_addr.sin_addr),
+					 client_addr.sin_port);
 
 		// Get the RFC 868 time and send it to the client
-		response_time = get_time();
-		sendto(sockfd, &response_time, sizeof(response_time), 0,
-					 (struct sockaddr *)&client_addr, addr_len);
-
-		// printf("Sent time to client: %s", ctime((time_t *)&response_time));
+		uint32_t time = get_rfc_time();
+		sendto(socket_fd, &time, sizeof(time), 0, (struct sockaddr *)&client_addr,
+					 addr_len);
 	}
 
-	close(sockfd);
+	close(socket_fd);
 	return 0;
 }
