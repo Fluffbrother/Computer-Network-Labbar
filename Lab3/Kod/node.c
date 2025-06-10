795e365faaf603b83f2815f8da7b292279090ae2
Fluffbrother <danielv.brynne@gmail.com>
2025-06-10 15:32:24 +0200

    Made a cost min function for updating the table

--- Lab3/Kod/node.c
+++ Lab3/Kod/node.c
@@ -6,33 +6,52 @@
 #include "sim_engine.h"
 #include "util.h"
 
+#define SIZE 4
+
+void cost_min(struct distance_table *table, int node){
+	int costmin[SIZE];
+		for(int i = 0; i < SIZE; i++){
+			int min  = INF;
+			for(int j = 0; j < SIZE; j++){
+				int cost;
+				if(j == node){
+					cost = table->costs[node][i];
+				}
+				else{
+					cost = table->costs[node][j] + table->costs[j][i];
+				}
+				if(cost < min){
+					min = cost;
+				}
+			}
+			costmin[i] = min;
+		}
+		for(int i = 0; i < SIZE; i++){
+			if (i == node || is_neighbor(i, node) != 1) {
+				continue;
+			}
+			struct rtpkt packet;
+			packet.sourceid = node;
+			packet.destid = i;
+			
+			for(int j = 0; j < SIZE; j++){
+				packet.mincost[j] = costmin[i];
+			}
+			tolayer2(packet);	
+		}
+}
+
 void rtinit(struct distance_table *table, int node) {
 	// Tables are already initialized
 	printdt(table, node);
-
-	for (int i = 0; i < 4; i++) {
-		if (i == node || is_neighbor(i, node) != 1) {
-			continue;
-		}
-		tolayer2((struct rtpkt){
-				.sourceid = node,
-				.destid = i,
-				.mincost =
-						{
-								table->costs[node][0],
-								table->costs[node][1],
-								table->costs[node][2],
-								table->costs[node][3],
-						},
-		});
-	}
+	cost_min(table, node);	
 }
 
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
-	static bool updated = false; 
+	bool updated = false; 
 	int send = pkt->sourceid;
 
-	for(int i = 0; i < 4; i++){
+	for(int i = 0; i < SIZE; i++){
 		int new_cost = table->costs[send][node] + pkt->mincost[i];
 
 		if(new_cost < table->costs[send][node]){
@@ -40,6 +59,10 @@ void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
 			updated = true;
 		}
 	}
+	if(updated){
+		cost_min(table, node);
+	}
 	// printdt(table, node);
 	debug("UPDATE: %i\n", node);
 }
+
